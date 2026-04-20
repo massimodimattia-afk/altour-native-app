4f72e14df1c8cda89b263f2747951292b25a08e6
massimodimattia-afk <massimo.dimattia@gmail.com>
2026-04-20 09:23:43 +0200

    aggiunte icone

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,15 @@
-import { View, Text } from "react-native";
+import { Image, View, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-lg font-bold">Home</Text>
+    <View className="flex-1 items-center justify-center bg-[#5aaadd]">
+      {/* Questo caricherà il file fisico che hai messo in assets */}
+      <Image 
+        source={require('../../assets/icon.png')} 
+        style={{ width: 250, height: 250 }}
+        resizeMode="contain"
+      />
+      <Text className="text-white font-bold mt-4 text-xl">Altour Italy</Text>
     </View>
   );
 }
